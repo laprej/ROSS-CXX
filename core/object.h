2442471953cef8280ed4b1e9a81773dbedf20881
Justin M. LaPre <justin@lapre.com>
2016-07-18 16:19:47 -0400

    Added SP_wrapper support.

--- core/object.h
+++ core/object.h
@@ -4,6 +4,9 @@
 #ifndef Object_h
 #define Object_h
 
+// BOOST_POOL_NO_MT turns off multithreading
+#define BOOST_POOL_NO_MT
+#include <boost/pool/pool_alloc.hpp>
 #include <memory>
 
 namespace object
@@ -36,5 +39,32 @@ namespace object
         friend std::unique_ptr<X> object::clone(const X&);
     };
 }
+
+template <typename T>
+class SP_wrapper
+{
+    bool newly_cloned = {true};
+    std::shared_ptr<T> ptr;
+
+public:
+    void set(const T& x)
+    {
+        if (newly_cloned) {
+            newly_cloned = false;
+            ptr = std::allocate_shared<T>(boost::fast_pool_allocator<T>(), x);
+        }
+        else {
+            *ptr = x;
+        }
+    }
+    T* get() const
+    {
+        return ptr.get();
+    }
+    std::shared_ptr<T>& getSP() { return ptr; }
+
+    SP_wrapper() = default;
+    SP_wrapper(const SP_wrapper &x) : newly_cloned(true), ptr(x.ptr) { }
+};
 
 #endif /* Object_h */
